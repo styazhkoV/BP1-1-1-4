ae6c0ef79f6d0c3194ae4237d6ca42357473cb29
styazhkoV <alexsandr.styazhkov@gmail.com>
2021-04-05 22:34:08 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BP1_1_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 2;
+            //int b = 3;
+            //int c = b / a;
+            //double d = (double)b / a;
+            //Console.WriteLine(d);
+
+            double angle = 3.14159;
+            //Console.WriteLine(Math.Sin(angle));
+            //Console.WriteLine(Math.Pow(2, 3));
+            //int a = 2;
+            //int b = 4;
+            //var c = a * b;
+            //Console.WriteLine(c);
+            //var d = c.ToString();
+            //var a = 2;
+            //var b = 4;
+            //var c = 6;
+            //a += 2;
+            //b *= 3;
+            //a++; увеличивает значение на единицу
+            //var a = 2;
+            //Console.WriteLine(++a); возвращает увеличенное значение
+            //Console.WriteLine(a);
+            //a = 2;
+            //Console.WriteLine(a++); возвращает уменьшенное значение
+            //Console.WriteLine(a);
+            int a = 2;
+            Console.WriteLine(a++ + ++a);
+
+          
+        }
+    }
+}
